fad39646c09dfbd578d793cc67331848c6610ac1
Kyle McCracken <kmccracken150@gmail.com>
2023-01-24 16:03:53 -0500

    add outline to modal, shorten link text in modal, other CSS changes

--- src/client/components/Modal.tsx
+++ src/client/components/Modal.tsx
@@ -9,48 +9,48 @@ interface ImodalProps {
 const Modal = (props: ImodalProps) => {
   return (
     <div className='modal-background' onClick={props.closeModal}>
-      <div className='modal-card' onClick={(e) => e.stopPropagation()}>
-        <div className='modal-top'>
-          <div className='modal-common-name'>
-            {props.modalContent.name || props.modalContent.scientificName}
+      <div className='modal-card-outline'>
+        <div className='modal-card' onClick={(e) => e.stopPropagation()}>
+          <div className='modal-top'>
+            <div className='modal-common-name'>
+              {props.modalContent.name || props.modalContent.scientificName}
+            </div>
+            <div className='modal-close' onClick={props.closeModal}>
+              ✖
+            </div>
           </div>
-          <div className='modal-close' onClick={props.closeModal}>
-            ✖
+          <div className='modal-card-img'>
+            <img src={props.modalContent.pictureUrl} />
           </div>
-        </div>
-        <div className='modal-card-img'>
-          <img src={props.modalContent.pictureUrl} />
-        </div>
-        <div className='modal-summary-label'>
-          <div className='modal-scientific-name'>
-            ({props.modalContent.scientificName})
-          </div>
-          <div>
-            <a
-              href={`https://www.inaturalist.org/taxa/${props.modalContent.taxaId}`}
-              target='_blank'
-            >
-              View on iNaturalist ↪
-            </a>
-          </div>
-          <div className='modal-count'>
-            {props.modalContent.found ? (
+          <div className='modal-summary-label'>
+            <div className='modal-scientific-name'>
+              ({props.modalContent.scientificName})
+            </div>
+            <div>
               <a
-                href={`https://www.inaturalist.org/observations?d1=${props.modalContent.queryInfo.curD1}&d2=${props.modalContent.queryInfo.curD2}&place_id=122840&taxon_id=${props.modalContent.taxaId}&verifiable=any`}
+                href={`https://www.inaturalist.org/taxa/${props.modalContent.taxaId}`}
                 target='_blank'
               >
-                View {props.modalContent.count} Current Observations on
-                iNaturalist ↪
+                View on iNaturalist ↪
               </a>
-            ) : (
-              <a
-                href={`https://www.inaturalist.org/observations?month=${props.modalContent.queryInfo.baselineMonth}&place_id=122840&taxon_id=${props.modalContent.taxaId}&verifiable=any`}
-                target='_blank'
-              >
-                View {props.modalContent.count} Past Observations on iNaturalist
-                ↪
-              </a>
-            )}
+            </div>
+            <div className='modal-count'>
+              {props.modalContent.found ? (
+                <a
+                  href={`https://www.inaturalist.org/observations?d1=${props.modalContent.queryInfo.curD1}&d2=${props.modalContent.queryInfo.curD2}&place_id=122840&taxon_id=${props.modalContent.taxaId}&verifiable=any`}
+                  target='_blank'
+                >
+                  View {props.modalContent.count} Current Obs. on iNaturalist ↪
+                </a>
+              ) : (
+                <a
+                  href={`https://www.inaturalist.org/observations?month=${props.modalContent.queryInfo.baselineMonth}&place_id=122840&taxon_id=${props.modalContent.taxaId}&verifiable=any`}
+                  target='_blank'
+                >
+                  View {props.modalContent.count} Past Obs. on iNaturalist ↪
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
