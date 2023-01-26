e58dba52c75b6ee9bbe3d4d2fc1687ea39788231
Kyle McCracken <kmccracken150@gmail.com>
2023-01-25 22:58:42 -0500

    update db env var name to be specific

--- src/db/db-connect.ts
+++ src/db/db-connect.ts
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 //travel app database
-const PG_URI = process.env.PG_URI;
+const PG_URI = process.env.CNC_PG_URI;
 
 // create a new pool here using the connection string above
 const pool = new Pool({
