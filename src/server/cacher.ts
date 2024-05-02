ccd8d1430bc4e7dd487212feca55fd164ee1f235
Kyle McCracken <kmccracken150@gmail.com>
2024-05-01 20:22:18 -0400

    fix accidentally using current project for previous query

--- src/server/cacher.ts
+++ src/server/cacher.ts
@@ -18,7 +18,9 @@ const challenge = process.env.PROJECT_ID
 // queries
 const queries: Object = {
   baseline: `https://api.inaturalist.org/v1/observations/species_counts?place_id=122840&month=${process.env.BASELINE_MONTH}&lrank=species&hrank=species&per_page=1000`,
-  previous: `https://api.inaturalist.org/v1/observations/species_counts?${challenge}&d1=${process.env.PREVIOUS_D1}&d2=${process.env.PREVIOUS_D2}&lrank=species&hrank=species&per_page=500`,
+  previous: `https://api.inaturalist.org/v1/observations/species_counts?${'place_id=122840'}&d1=${
+    process.env.PREVIOUS_D1
+  }&d2=${process.env.PREVIOUS_D2}&lrank=species&hrank=species&per_page=500`,
   current: `https://api.inaturalist.org/v1/observations/species_counts?${challenge}&d1=${process.env.CURRENT_D1}&d2=${process.env.CURRENT_D2}&lrank=species&hrank=species&per_page=500`,
 };
 
