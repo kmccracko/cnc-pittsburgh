1631cafe0ffb60a7023147faab397965bbf997f2
Kyle McCracken <kmccracken150@gmail.com>
2025-04-22 00:49:55 -0400

    implement test database, add "Is it still missing?" link to missing species dialogs

--- src/db/db-connect.ts
+++ src/db/db-connect.ts
@@ -1,15 +1,44 @@
 const { Pool } = require('pg');
 
-//travel app database
-const PG_URI = process.env.CNC_PG_URI;
+const createPool = () => {
+  let poolConfig;
+  if (process.env.NODE_ENV === 'development') {
+    poolConfig = {
+      host: process.env.DEV_HOST_IP,
+      port: 5434,
+      user: 'postgres',
+      password: 'password',
+      database: 'cncpgh_dev'
+    };
+  } else {
+    poolConfig = {
+      connectionString: process.env.CNC_PG_URI,
+    };
+  }
 
-// create a new pool here using the connection string above
-const pool = new Pool({
-  connectionString: PG_URI,
-});
+  let pool: any;
+  try {
+    console.log('Attempting to connect with config:', {
+      ...poolConfig,
+      password: '*****' // Hide password in logs
+    });
+    pool = new Pool(poolConfig);
+    console.log(`Connected to ${poolConfig.database} DB!`);
+  } catch (error) {
+    console.error(`Could not connect to ${poolConfig.database} DB: `, error);
+  }
+  return pool;
+}
 
-// Adding some notes about the database here will be helpful for future you or other developers.
-// Schema for the database can be found below:
+const pool = createPool();
+
+// Test query: select * from baseline;
+// const testQuery = async () => {
+//   console.log('testQuery');
+//   const result = await pool.query('SELECT * FROM baseline;');
+//   console.log(result.rows.length);
+// };
+// testQuery();
 
 // We export an object that contains a property called query,
 // which is a function that returns the invocation of pool.query() after logging the query
